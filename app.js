4dc9f7cc1d648a8782845a608da5ad6f452638f4
Rafael <rlbalocco0@gmail.com>
2023-08-02 18:07:40 -0300

    Adding somre progress :wq

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 const chalk = require ('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const getnotes = require('./notes')
+yargs.version('1.1.0')
 
-const msg = getnotes()
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        notes.addNote(argv.title, argv.body)
+    }
+})
 
-console.log(chalk.green.bold.underline('Success'));
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log('listing out all notes!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note!')
+    }
+})
+
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,5 +1,76 @@
-const getnotes = function (){
+const fs = require('fs')
+const chalk = require ('chalk')
+
+
+const getNotes = function (){
     return 'Your notes...'
 }
 
-module.exports = getnotes
+const addNote = function(title, body){
+
+    const notes = loadNotes()
+
+    const duplicateNotes = notes.filter(function (note){ //compara os objetos para ver se tem algum repetido
+        return note.title == title  // compara o titulo que sera inserido com cada titulo que ja está inserido na lista
+    })
+
+    if (duplicateNotes.length === 0){ //se o array criado que teria essas repetições estiver vazio, não há repetição, podemos fazer push 
+
+        notes.push({
+            title:title, //load the data passed by argument to a new arg
+            body:body
+        })
+
+        saveNotes(notes)
+        console.log(chalk.bgGreen.black("Note saved at the list"))
+    }
+
+    else{
+        console.log(chalk.bgRed.black("Note title already exists"))
+    }
+ 
+}
+
+const saveNotes = function (notes){
+
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+
+}
+
+const loadNotes = function (){ //retorna array das notas
+    try{
+
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)   
+
+    }catch(e){
+
+        return []
+
+    }
+}
+
+const removeNote = function(title){
+
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function(note){
+        if (note.title === title){
+            console.log(chalk.black.bgGreen("Note removed!"))  
+            return false       
+        }
+        else{
+            return true
+        }
+    })
+    if(JSON.stringify(notes)==JSON.stringify(notesToKeep)) console.log(chalk.black.bgRed("Note doesn't exist!"))
+
+    saveNotes(notesToKeep)
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
